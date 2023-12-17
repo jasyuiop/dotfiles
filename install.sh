d9f2cd20bb22853a2b90c4acc948e178d104e1e7
M.Emre Nefesli <emrenefesli@outlook.com>
2023-12-17 21:48:30 +0300

    refactored install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
-# Get config on github and make as git bare repo
-git clone --bare git@github.com:jasyuiop/dotfiles.git $HOME/.cfg
+
+# Define the GitHub repository URL and target location
+repo_url="git@github.com:jasyuiop/dotfiles.git"
+cfg_dir="$HOME/.cfg"
+backup_dir="$HOME/.config-backup"
+
+# Clone the configuration from GitHub and set it as a bare Git repository
+git clone --bare "$repo_url" "$cfg_dir"
+
+# Define the dotfiles function
 function dotfiles {
-	/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+	/usr/bin/git --git-dir="$cfg_dir/" --work-tree="$HOME" "$@"
 }
-mkdir -p .config-backup
-dotfiles checkout
-if [ $? = 0 ]; then
+
+# Create the .config-backup directory
+mkdir -p "$backup_dir"
+
+# Check if the checkout was successful
+if dotfiles checkout; then
 	echo "Checked out config."
 else
 	echo "Backing up pre-existing dot files."
-	dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+
+	# Move dot files to the .config-backup directory
+	dotfiles checkout 2>&1 | awk '/^\s+\./ {print $1}' | xargs -I{} mv {} "$backup_dir/{}"
 fi
+
+# Run the dotfiles checkout command again and set status.showUntrackedFiles to no
 dotfiles checkout
 dotfiles config status.showUntrackedFiles no
